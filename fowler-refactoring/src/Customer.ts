a9999e3638bd5a291633b28b0d6f62a4bac57b3f
paulslusarz <paulslusarz@carfax.com>
2023-11-12 21:15:36 -0600

    move method to Rental

--- fowler-refactoring/src/Customer.ts
+++ fowler-refactoring/src/Customer.ts
@@ -14,7 +14,7 @@ export class Customer extends DomainObject {
         let totalAmount = 0
         let frequentRenterPoints = 0
         this.rentals.forEach((rental) => {
-            let thisAmount = this.amountOf(rental);
+            let thisAmount = rental.amountOf();
             totalAmount += thisAmount
             frequentRenterPoints++
             if ((rental.tape.movie.priceCode === PriceCode.NEW_RELEASE) && (rental.daysRented > 1)) {
@@ -27,25 +27,5 @@ export class Customer extends DomainObject {
         return result
     }
 
-    private amountOf(rental: Rental) {
-        let thisAmount = 0
-        switch (rental.tape.movie.priceCode) {
-            case PriceCode.REGULAR:
-                thisAmount += 2
-                if (rental.daysRented > 2) {
-                    thisAmount += (rental.daysRented - 2) * 1.5
-                }
-                break;
-            case PriceCode.NEW_RELEASE:
-                thisAmount += rental.daysRented * 3
-                break;
-            case PriceCode.CHILDRENS:
-                thisAmount += 1.5
-                if (rental.daysRented > 3) {
-                    thisAmount += (rental.daysRented - 3) * 1.5
-                }
-                break;
-        }
-        return thisAmount;
-    }
+
 }
--- fowler-refactoring/src/Rental.ts
+++ fowler-refactoring/src/Rental.ts
@@ -1,8 +1,31 @@
 import {DomainObject} from "./DomainObject";
 import {Tape} from "./Tape";
+import {PriceCode} from "./Movie";
 
 export class Rental extends DomainObject {
     constructor(public tape : Tape, public daysRented : number) {
         super();
+    }
+
+    amountOf() {
+        let thisAmount = 0
+        switch (this.tape.movie.priceCode) {
+            case PriceCode.REGULAR:
+                thisAmount += 2
+                if (this.daysRented > 2) {
+                    thisAmount += (this.daysRented - 2) * 1.5
+                }
+                break;
+            case PriceCode.NEW_RELEASE:
+                thisAmount += this.daysRented * 3
+                break;
+            case PriceCode.CHILDRENS:
+                thisAmount += 1.5
+                if (this.daysRented > 3) {
+                    thisAmount += (this.daysRented - 3) * 1.5
+                }
+                break;
+        }
+        return thisAmount;
     }
 }
