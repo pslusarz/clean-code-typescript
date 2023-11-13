1b64b3b628106ef277ff2211f9e9af91468aebfa
paulslusarz <paulslusarz@carfax.com>
2023-11-13 00:51:41 -0600

    frequent renter points on new release

--- fowler-refactoring/src/NewReleasePrice.ts
+++ fowler-refactoring/src/NewReleasePrice.ts
@@ -1,7 +1,16 @@
 import {Price} from "./Price";
+import {PriceCode} from "./Movie";
 
 export class NewReleasePrice extends Price {
     charge(daysRented : number) : number {
         return daysRented * 3
+    }
+
+    frequentRenterPoints(daysRented: number) {
+        if (daysRented > 1) {
+            return 2
+        } else {
+            return 1
+        }
     }
 }
