ebd14bcff1e765b375582386752871323d6a4460
paulslusarz <paulslusarz@carfax.com>
2023-11-12 22:48:44 -0600

    extract charge

--- fowler-refactoring/src/Customer.ts
+++ fowler-refactoring/src/Customer.ts
@@ -11,17 +11,24 @@ export class Customer extends DomainObject {
 
     statement() {
         let result = `Rental Record for ${this.name}\n`
-        let totalAmount = 0
+
         let frequentRenterPoints = 0
         this.rentals.forEach((rental) => {
-            totalAmount += rental.charge()
             frequentRenterPoints += rental.frequentRenterPoints()
-
             result += `\t${rental.tape.movie.name}\t${(rental.charge())}\n`
         })
-        result += `Amount owed is ${totalAmount}\n`;
+        result += `Amount owed is ${this.charge()}\n`;
         result += `You earned ${frequentRenterPoints} frequent renter points`
         return result
+    }
+
+    charge(): number {
+        // let result = 0
+        // this.rentals.forEach((rental) => {
+        //     result += rental.charge()
+        // })
+        // return result
+        return this.rentals.reduce((acc, rental) => acc + rental.charge(), 0)
     }
 
 
