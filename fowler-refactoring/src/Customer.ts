717596ba872c534c314f00e9e88a4e7e1cd36fc7
paulslusarz <paulslusarz@carfax.com>
2023-11-12 21:12:17 -0600

    extract method

--- fowler-refactoring/src/Customer.ts
+++ fowler-refactoring/src/Customer.ts
@@ -14,24 +14,7 @@ export class Customer extends DomainObject {
         let totalAmount = 0
         let frequentRenterPoints = 0
         this.rentals.forEach((rental) => {
-            let thisAmount = 0
-            switch (rental.tape.movie.priceCode) {
-                case PriceCode.REGULAR:
-                    thisAmount += 2
-                    if (rental.daysRented > 2) {
-                        thisAmount += (rental.daysRented - 2) * 1.5
-                    }
-                    break;
-                case PriceCode.NEW_RELEASE:
-                    thisAmount += rental.daysRented * 3
-                    break;
-                case PriceCode.CHILDRENS:
-                    thisAmount += 1.5
-                    if (rental.daysRented > 3) {
-                        thisAmount += (rental.daysRented - 3) * 1.5
-                    }
-                    break;
-            }
+            let thisAmount = this.amountOf(rental);
             totalAmount += thisAmount
             frequentRenterPoints++
             if ((rental.tape.movie.priceCode === PriceCode.NEW_RELEASE) && (rental.daysRented > 1)) {
@@ -42,5 +25,27 @@ export class Customer extends DomainObject {
         result += `Amount owed is ${totalAmount}\n`;
         result += `You earned ${frequentRenterPoints} frequent renter points`
         return result
+    }
+
+    private amountOf(rental: Rental) {
+        let thisAmount = 0
+        switch (rental.tape.movie.priceCode) {
+            case PriceCode.REGULAR:
+                thisAmount += 2
+                if (rental.daysRented > 2) {
+                    thisAmount += (rental.daysRented - 2) * 1.5
+                }
+                break;
+            case PriceCode.NEW_RELEASE:
+                thisAmount += rental.daysRented * 3
+                break;
+            case PriceCode.CHILDRENS:
+                thisAmount += 1.5
+                if (rental.daysRented > 3) {
+                    thisAmount += (rental.daysRented - 3) * 1.5
+                }
+                break;
+        }
+        return thisAmount;
     }
 }
