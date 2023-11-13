c9cf4e312959cd942ba72ded9a1c6e10aaa4a000
paulslusarz <paulslusarz@carfax.com>
2023-11-13 00:49:54 -0600

    charge is now abstract on price, moved all logic to subclasses

--- fowler-refactoring/src/ChildrensPrice.ts
+++ fowler-refactoring/src/ChildrensPrice.ts
@@ -1,5 +1,12 @@
 import {Price} from "./Price";
 
 export class ChildrensPrice extends Price {
+    charge(daysRented : number) : number {
+        let result = 1.5
+        if (daysRented > 3) {
+            result += (daysRented - 3) * 1.5
+        }
+        return result
+    }
 
 }
--- fowler-refactoring/src/Price.ts
+++ fowler-refactoring/src/Price.ts
@@ -3,28 +3,11 @@ import {RegularPrice} from "./RegularPrice";
 import {ChildrensPrice} from "./ChildrensPrice";
 import {PriceCode} from "./Movie";
 
-export class Price {
+export abstract class Price {
    constructor(public priceCode : PriceCode) {
    }
 
-   charge(daysRented : number) {
-      let result = 0
-      switch (this.priceCode) {
-         case PriceCode.REGULAR:
-            result += 2
-            if (daysRented > 2) {
-               result += (daysRented - 2) * 1.5
-            }
-            break;
-         case PriceCode.CHILDRENS:
-            result += 1.5
-            if (daysRented > 3) {
-               result += (daysRented - 3) * 1.5
-            }
-            break;
-      }
-      return result;
-   }
+   abstract charge(daysRented : number): number
 
    frequentRenterPoints(daysRented: number) {
       if ((this.priceCode === PriceCode.NEW_RELEASE) && (daysRented > 1)) {
